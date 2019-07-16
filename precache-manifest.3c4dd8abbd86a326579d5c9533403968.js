1f41d232b61096e8519a15e8f08c98b5f08da466
Luke Markham <lukeeliotmarkham2@hotmail.com>
2019-07-16 10:12:37 +0700

    Updates

---
+++ precache-manifest.3c4dd8abbd86a326579d5c9533403968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0ee01c5cacbd866e0a56c3b6c4725d",
+    "url": "/online-newspaper/index.html"
+  },
+  {
+    "revision": "2d0991503adb8ec7a0c5",
+    "url": "/online-newspaper/static/css/main.a379d2fa.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/online-newspaper/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2d0991503adb8ec7a0c5",
+    "url": "/online-newspaper/static/js/main.b8c99154.chunk.js"
+  },
+  {
+    "revision": "1fa9af40a31de4957bad",
+    "url": "/online-newspaper/static/js/runtime~main.477aaccc.js"
+  }
+]);
